151baef72508480f2e025bfd57c2082edfd42ab6
Nova67 <innovagames75@gmail.com>
2023-03-18 23:16:04 -0400

    formulario estilizado y colores para diferenciar

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-// --------------- VARIABLES ---------------
-var resultContainer = document.getElementById('qr-results');
+// --------------- LECTOR QR ---------------
+var resultContainer = document.getElementById("qr-results");
 var countResults = 0;
 
-// --------------- FUNCIONES ---------------
 // lo que pasa cuando se detecta el QR
 function onScanSuccess(decodedText, decodedResult) {
 	// handle the scanned code as you like, for example:
-    // mostrar en consola
+	// mostrar en consola
 	console.log(`Code matched = ${decodedText}`, decodedResult);
-    // sumar contador
-    ++countResults;
-    // mostrar en la pagina lo que se detecta
-    resultContainer.innerHTML += `<div>[${countResults}] - ${decodedText}</div>`;
+	// sumar contador
+	++countResults;
+	// mostrar en la pagina lo que se detecta
+	resultContainer.innerHTML += `<div>[${countResults}] - ${decodedText}</div>`;
 }
 
 // lo que pasa cuando no se detecta nada
@@ -31,3 +30,31 @@ let scanner = new Html5QrcodeScanner(
 
 // ejecutar objeto
 scanner.render(onScanSuccess, onScanFailure);
+
+// --------------- FORMULARIO CLIENTE ---------------
+var fcliente = document.getElementById("cliente-info");
+
+var nombre = document.getElementById("c-nombre").value;
+var apellido = document.getElementById("c-apellidos").value;
+var empresa = document.getElementById("c-empresa").value;
+var direccion = document.getElementById("c-direccion").value;
+var email = document.getElementById("c-email").value;
+var telefono = document.getElementById("c-telefono").value;
+var departamento = document.getElementById("c-departamento").value;
+
+// escuchando cuando se envie el formulario
+fcliente.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	validarDatos();
+
+	console.log(
+		nombre,
+		apellido,
+		empresa,
+		direccion,
+		email,
+		telefono,
+		departamento
+	);
+});
